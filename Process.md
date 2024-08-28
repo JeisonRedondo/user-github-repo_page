470b41212a797c12c4e46de4e1487f9f9b0f219f
Jeison Redondo <jeida16@yahoo.es>
2024-08-27 20:51:58 -0500

    [ADD,CHANGE](Funcionality)Create function for obtain repo information about user github

--- Process.md
+++ Process.md
@@ -8,11 +8,16 @@ Hola Jeison aqui pondremos nuestros pasos para generar la aplicación de manera 
 
 ## TODO
 
-- [] (Diseño)Generar una maqueta para probar las funcionalidades de la pagina.
 - [] (Funcionalidad) Empezar a implementar la api de github y hacer pruebas.
+  - [] Implementar los filtros para obtener nombres de API.
+  - [] Agregar la funcionalidad a el template.
+- [] (Diseño) Agregar detalles de Estilo tipo Github.
+  - [] Generar bordes para los contenedores.
+  - [] Agregar extras para los lados.
 
 ## Partes desarrolladas
 
 - [x] Inicialización de la aplicación.
 - [x] Agregar el .md para manejar el proceso y progreso de la aplicación.
 - [x] (Diseño)Crear la layout para la pagina.
+- [x] (Diseño)Generar una maqueta para probar las funcionalidades de la pagina.
--- src/services/services-github.js
+++ src/services/services-github.js
@@ -4,14 +4,32 @@ import axios from "axios";
 const githubUser = async (username) => {
   try {
     const response = await axios.get(
-      `https://api.github.com/users/${username}`,
+      `https://api.github.com/users/${username}/repos`,
     );
     return response.data;
   } catch (error) {
-    console.error("Error al obtener el usuario de github", error);
+    console.error("Error getting Github User", error);
     return null;
   }
 };
 
-const respuesta = await githubUser("JeisonRedondo");
-console.log(respuesta);
+async function githubUserRepo() {
+  const respuesta = await githubUser("JeisonRedondo");
+
+  const RepoArray = [];
+
+  for (let index = 0; index < respuesta.length; index++) {
+    let RepoObject = {
+      name: respuesta[index].name,
+      description: respuesta[index].description,
+      url: respuesta[index].url,
+    };
+    RepoArray.push(RepoObject);
+  }
+
+  console.log(RepoArray);
+
+  return RepoArray;
+}
+
+githubUserRepo();
